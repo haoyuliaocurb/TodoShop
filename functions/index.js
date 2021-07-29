aa41f88a818f9f1e0d9690ea7dbe1c7b1dea3811
Hao-yu Liao <jeffery84115@gmail.com>
2021-07-29 17:27:22 +0800

    修正 fix/notProperlyDeployFunctions 錯誤

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
-
-const Promise = require("promise");
 const algoliasearch = require("algoliasearch");
 
 const ALGOLIA_ID = functions.config().algolia.appid;
@@ -96,25 +94,33 @@ exports.deleteProductFromAlgolia = functions
           .catch((err) => console.log("ERROR ALGOLIA product REMOVE", err));
     });
 
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection("messages")
-      .add({original: original});
-  // Send back a message that we"ve successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
+// exports.addMessage = functions.https.onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into Firestore using the Firebase Admin SDK.
+//   const writeResult = await admin.firestore().collection("messages")
+//       .add({original: original});
+//   // Send back a message that we"ve successfully written the message
+//   res.json({result: `Message with ID: ${writeResult.id} added.`});
+// });
 
-exports.makeUppercase = functions.firestore.document("/messages/{documentId}")
-    .onCreate((snap, context) => {
-      // Grab the current value of what was written to Firestore.
-      const original = snap.data().original;
+// exports.makeUppercase = functions
+//     .firestore.document("/messages/{documentId}")
+//     .onCreate((snap, context) => {
+//       // Grab the current value of what was written to Firestore.
+//       const original = snap.data().original;
 
-      // Access the parameter `{documentId}` with `context.params`
-      functions.logger.log("Uppercasing", context.params.documentId, original);
+//       // Access the parameter `{documentId}` with `context.params`
+//       functions
+//           .logger.log("Uppercasing", context.params.documentId, original);
 
-      const uppercase = original.toUpperCase();
+//       const uppercase = original.toUpperCase();
 
-      return snap.ref.set({uppercase}, {merge: true});
-    });
+//       return snap.ref.set({uppercase}, {merge: true});
+//     });
+
+exports.helloWorld = (req, res) => {
+  console.log("I am a log entry!");
+  console.error("I am an error!");
+  res.end();
+};
