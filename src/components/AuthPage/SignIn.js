31a72cf89a308f952d099af3b3ce82a098869a52
Hao-yu Liao <jeffery84115@gmail.com>
2021-08-05 03:27:12 +0800

    AuthPage: 新增註冊帳號功能

--- src/components/AuthPage/SignIn.js
+++ src/components/AuthPage/SignIn.js
@@ -1,19 +1,118 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-vars */
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { uiConfig, StyledFirebaseAuth, firebase } from '../../utils/firebase/firebase-services';
+import {
+  uiConfig,
+  StyledFirebaseAuth,
+  firebase,
+  auth,
+} from '../../utils/firebase/firebase-services';
 
 import StyledSignIn from '../../styles/AuthPage/StyledSignIn';
 
 const SignIn = ({ isSignIn }) => {
   const history = useHistory();
   const test = window.localStorage.getItem('TodoShopPreLocationFromSignIn');
-  // console.log('test: ', test);
-
+  const [isShowSignUp, setIsShowSignUp] = useState(0);
+  const [signUpInputValue, setSignUpInputValue] = useState({ email: '', password: '' });
+  const handleButtonSignUp = (e) => {
+    e.preventDefault();
+    const { email, password } = signUpInputValue;
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('successfully sign up');
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === 'auth/weak-password') {
+          console.log('The password is too weak.');
+        } else {
+          console.log(errorMessage);
+        }
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    // console.log('signUpInputValue: ', signUpInputValue);
+  }, [signUpInputValue]);
   return (
     <StyledSignIn>
       {!isSignIn ? (
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        !isShowSignUp ? (
+          <div className="background">
+            <div className="dashBoard">
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+              <p className="textToggle">
+                <span>還沒有帳號嗎？</span>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setIsShowSignUp(1);
+                  }}
+                >
+                  註冊
+                </button>
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="background-signUp">
+            <div className="dashBoard-signUp">
+              <form className="form-signUp" onSubmit={handleButtonSignUp}>
+                <h1>註冊帳號</h1>
+                <label htmlFor="textEmail">
+                  <input
+                    placeholder="Email"
+                    id="textEmail"
+                    value={signUpInputValue.email}
+                    onInput={(e) => {
+                      setSignUpInputValue((preSignUpInputValue) => {
+                        const newSignUpInputValue = {
+                          ...preSignUpInputValue,
+                          email: e.target.value,
+                        };
+                        return newSignUpInputValue;
+                      });
+                    }}
+                  />
+                </label>
+                <label htmlFor="textPassword">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    id="textPassword"
+                    value={signUpInputValue.password}
+                    onInput={(e) => {
+                      setSignUpInputValue((preSignUpInputValue) => {
+                        const newSignUpInputValue = {
+                          ...preSignUpInputValue,
+                          password: e.target.value,
+                        };
+                        return newSignUpInputValue;
+                      });
+                    }}
+                  />
+                </label>
+                <button type="submit">註冊</button>
+              </form>
+              <p className="textToggle-signUp">
+                <span>已經有帳號了嗎？</span>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setIsShowSignUp(0);
+                  }}
+                >
+                  登入
+                </button>
+              </p>
+            </div>
+          </div>
+        )
       ) : (
         <p>redirect ...</p>
       )}
--- src/styles/AuthPage/StyledSignIn.js
+++ src/styles/AuthPage/StyledSignIn.js
@@ -1,12 +1,139 @@
 import styled from '@emotion/styled/macro';
+import { styledVariables } from '../app/cssMaterial';
 
 const StyledSignIn = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
   align-items: center;
-  > #firebaseui_container {
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 60%;
+    background-color: ${styledVariables.color.pink400};
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    @media (max-width: 600px) {
+      justify-content: center;
+    }
+  }
+  .background-signUp {
+    position: absolute;
+    width: 100%;
+    height: 60%;
+    background-color: ${styledVariables.color.pink400};
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    @media (max-width: 600px) {
+      justify-content: center;
+    }
+  }
+  .dashBoard {
+    width: 300px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-wrap: wrap;
+  }
+  .dashBoard-signUp {
+    width: 300px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-wrap: wrap;
+  }
+  .textToggle {
+    width: 100%;
+    text-align: center;
+    margin-top: 40px;
+    > * {
+      font-size: 12px;
+      color: ${styledVariables.color.white};
+    }
+    > span {
+    }
+    > button {
+      margin-left: 6px;
+      text-decoration: underline;
+    }
+    /* position: absolute;
+    bottom: 50px;
+    z-index: 2; */
+  }
+  .textToggle-signUp {
+    width: 100%;
+    text-align: center;
+    margin-top: 40px;
+    > * {
+      font-size: 12px;
+      color: ${styledVariables.color.white};
+    }
+    > span {
+    }
+    > button {
+      margin-left: 6px;
+      text-decoration: underline;
+    }
+    /* position: absolute;
+    bottom: 50px;
+    z-index: 2; */
+  }
+  .form-signUp {
+    position: relative;
+    width: 226px;
+    height: 215px;
+    background-color: ${styledVariables.color.white};
+    border-radius: 6px;
+    box-shadow: 2px 2px 2px ${styledVariables.color.transGray30};
+    padding: 24px;
+    > h1 {
+      margin-bottom: 12px;
+    }
+    > label {
+      display: inline-block;
+      margin-top: 10px;
+      border-bottom: solid 1px ${styledVariables.color.gray200};
+      padding: 4px 0;
+      > input {
+        outline: none;
+        border: none;
+      }
+    }
+    > button {
+      width: 88px;
+      height: 36px;
+      padding: 0 16px;
+      background-color: ${styledVariables.color.pink400};
+      color: ${styledVariables.color.white};
+      border-radius: 100px;
+      position: absolute;
+      bottom: 24px;
+      right: 24px;
+      box-shadow: 2px 2px 2px ${styledVariables.color.transGray20};
+    }
+  }
+  #firebaseui_container {
+    /* position: relative;
+    right: 10%;
+    @media (max-width: 600px) {
+      right: unset;
+    } */
+  }
+  .firebaseui-form-actions {
+    button {
+      background-color: ${styledVariables.color.pink400};
+      border-radius: 100px;
+    }
+  }
+  .firebaseui-title {
+    font-size: 18px;
+  }
+  .firebaseui-container {
+    border-radius: 6px;
   }
 `;
 
